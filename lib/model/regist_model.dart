6abc8eb2bb095a353da9e3feb3c7e000107d6901
竹尾広章 <takeohiroaki@takeohihounoAir.kanax1.kt.home.ne.jp>
2020-05-08 23:40:18 +0900

    スライダーに

--- lib/model/regist_model.dart
+++ lib/model/regist_model.dart
@@ -8,6 +8,47 @@ enum Kokoro {
   veryGood,
 }
 
+extension KokoroExt on Kokoro {
+  String get description {
+    switch (this) {
+      case Kokoro.bad:
+        return "悪い";
+      case Kokoro.notSoGood:
+        return "いまいち";
+      case Kokoro.normal:
+        return "普通";
+      case Kokoro.good:
+        return "良い";
+      case Kokoro.veryGood:
+        return "最高！";
+      default:
+        return "";
+    }
+  }
+
+  int get value {
+    switch (this) {
+      case Kokoro.bad:
+        return 0;
+      case Kokoro.notSoGood:
+        return 1;
+      case Kokoro.normal:
+        return 2;
+      case Kokoro.good:
+        return 3;
+      case Kokoro.veryGood:
+        return 4;
+      default:
+        return 0;
+    }
+  }
+
+  static Kokoro fromDouble(double value) {
+    return Kokoro.values
+        .firstWhere((element) => element.value == value.toInt());
+  }
+}
+
 class RegistModel extends ChangeNotifier {
   Kokoro _kokoro = Kokoro.normal;
   String _comment = "";
@@ -16,6 +57,12 @@ class RegistModel extends ChangeNotifier {
 
   setComment(String comment) {
     this._comment = comment;
+  }
+
+  Kokoro getKokoro() => _kokoro;
+
+  setKokoro(Kokoro kokoro) {
+    this._kokoro = kokoro;
 
     notifyListeners();
   }
--- lib/ui/regist/regist.dart
+++ lib/ui/regist/regist.dart
@@ -5,8 +5,12 @@ import 'package:kokolog/ui/components/base_text.dart';
 import 'package:provider/provider.dart';
 
 class Regist extends StatelessWidget {
+  var _kokoro = 0.0;
+
   @override
   Widget build(BuildContext context) {
+    _kokoro = Provider.of<RegistModel>(context).getKokoro().value.toDouble();
+
     return Scaffold(
       appBar: AppBar(title: const Text('ココログ')),
       body: Column(
@@ -25,44 +29,62 @@ class Regist extends StatelessWidget {
 
   Widget _buildInputs(BuildContext context) {
     return Expanded(
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(
-              top: 50.0,
-            ),
-            child: Center(
-              child: BaseText(
-                text: '今日1日はどうでしたか？',
+      child: Padding(
+        padding: EdgeInsets.only(right: 50.0, left: 50.0),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                top: 50.0,
+              ),
+              child: Center(
+                child: BaseText(
+                  text: '今日1日はどうでしたか？',
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 50.0, right: 50.0, left: 50.0),
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Theme(
-                data: ThemeData(
-                  primaryColor: textFormPrimaryColor,
-                  primaryColorDark: textFormPrimaryColor,
-                ),
-                child: TextField(
-                  onSubmitted: Provider.of<RegistModel>(
-                    context,
-                    listen: false,
-                  ).setComment,
-                  decoration: InputDecoration(
-                    fillColor: textFormPrimaryColor,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Slider(
+                value: _kokoro,
+                min: 0,
+                max: 4,
+                divisions: 4,
+                label: KokoroExt.fromDouble(_kokoro).description,
+                onChanged: (double value) {
+                  Provider.of<RegistModel>(context, listen: false)
+                      .setKokoro(KokoroExt.fromDouble(value));
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 40.0),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Theme(
+                  data: ThemeData(
+                    primaryColor: textFormPrimaryColor,
+                    primaryColorDark: textFormPrimaryColor,
+                  ),
+                  child: TextField(
+                    onSubmitted: Provider.of<RegistModel>(
+                      context,
+                      listen: false,
+                    ).setComment,
+                    decoration: InputDecoration(
+                      fillColor: textFormPrimaryColor,
+                      hintText: "それはなぜですか？",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      contentPadding: EdgeInsets.all(10.0),
                     ),
-                    contentPadding: EdgeInsets.all(10.0),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
